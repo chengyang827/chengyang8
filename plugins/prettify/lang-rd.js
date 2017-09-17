15727e3a1b0cdf30f008ea02b6b28363da018672
chengyang827 <chengyang_827@163.com>
2017-09-17 13:46:40 +0800

    Site updated: 2017-09-17 13:46:27

--- plugins/prettify/lang-rd.js
+++ plugins/prettify/lang-rd.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 17 2017 12:40:11 GMT+0800 (中国标准时间)
+// build time:Sun Sep 17 2017 13:45:49 GMT+0800 (中国标准时间)
 PR["registerLangHandler"](PR["createSimpleLexer"]([[PR["PR_PLAIN"],/^[\t\n\r \xA0]+/,null,"\t\n\r  "],[PR["PR_COMMENT"],/^%[^\r\n]*/,null,"%"]],[[PR["PR_LITERAL"],/^\\(?:cr|l?dots|R|tab)\b/],[PR["PR_KEYWORD"],/^\\[a-zA-Z@]+/],[PR["PR_KEYWORD"],/^#(?:ifn?def|endif)/],[PR["PR_PLAIN"],/^\\[{}]/],[PR["PR_PUNCTUATION"],/^[{}()\[\]]+/]]),["Rd","rd"]);
 //rebuild by neat 
--- plugins/prettify/lang-wiki.js
+++ plugins/prettify/lang-wiki.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 17 2017 12:40:11 GMT+0800 (中国标准时间)
+// build time:Sun Sep 17 2017 13:45:49 GMT+0800 (中国标准时间)
 PR["registerLangHandler"](PR["createSimpleLexer"]([[PR["PR_PLAIN"],/^[\t \xA0a-gi-z0-9]+/,null,"\t  abcdefgijklmnopqrstuvwxyz0123456789"],[PR["PR_PUNCTUATION"],/^[=*~\^\[\]]+/,null,"=*~^[]"]],[["lang-wiki.meta",/(?:^^|\r\n?|\n)(#[a-z]+)\b/],[PR["PR_LITERAL"],/^(?:[A-Z][a-z][a-z0-9]+[A-Z][a-z][a-zA-Z0-9]+)\b/],["lang-",/^\{\{\{([\s\S]+?)\}\}\}/],["lang-",/^`([^\r\n`]+)`/],[PR["PR_STRING"],/^https?:\/\/[^\/?#\s]*(?:\/[^?#\s]*)?(?:\?[^#\s]*)?(?:#\S*)?/i],[PR["PR_PLAIN"],/^(?:\r\n|[\s\S])[^#=*~^A-Zh\{`\[\r\n]*/]]),["wiki"]);PR["registerLangHandler"](PR["createSimpleLexer"]([[PR["PR_KEYWORD"],/^#[a-z]+/i,null,"#"]],[]),["wiki.meta"]);
 //rebuild by neat 
