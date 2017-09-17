26e16404900e371e9197a258eadb1d4df7bfabdd
chengyang827 <chengyang_827@163.com>
2017-09-17 14:31:53 +0800

    Site updated: 2017-09-17 14:31:48

--- plugins/prettify/lang-go.js
+++ plugins/prettify/lang-go.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 17 2017 13:45:49 GMT+0800 (中国标准时间)
+// build time:Sun Sep 17 2017 14:31:10 GMT+0800 (中国标准时间)
 PR["registerLangHandler"](PR["createSimpleLexer"]([[PR["PR_PLAIN"],/^[\t\n\r \xA0]+/,null,"\t\n\r  "],[PR["PR_PLAIN"],/^(?:\"(?:[^\"\\]|\\[\s\S])*(?:\"|$)|\'(?:[^\'\\]|\\[\s\S])+(?:\'|$)|`[^`]*(?:`|$))/,null,"\"'"]],[[PR["PR_COMMENT"],/^(?:\/\/[^\r\n]*|\/\*[\s\S]*?\*\/)/],[PR["PR_PLAIN"],/^(?:[^\/\"\'`]|\/(?![\/\*]))+/i]]),["go"]);
 //rebuild by neat 
--- plugins/prettify/lang-pascal.js
+++ plugins/prettify/lang-pascal.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 17 2017 13:45:49 GMT+0800 (中国标准时间)
+// build time:Sun Sep 17 2017 14:31:10 GMT+0800 (中国标准时间)
 PR.registerLangHandler(PR.createSimpleLexer([[PR.PR_STRING,/^(?:\'(?:[^\\\'\r\n]|\\.)*(?:\'|$))/,null,"'"],[PR.PR_PLAIN,/^\s+/,null," \r\n\t "]],[[PR.PR_COMMENT,/^\(\*[\s\S]*?(?:\*\)|$)|^\{[\s\S]*?(?:\}|$)/,null],[PR.PR_KEYWORD,/^(?:ABSOLUTE|AND|ARRAY|ASM|ASSEMBLER|BEGIN|CASE|CONST|CONSTRUCTOR|DESTRUCTOR|DIV|DO|DOWNTO|ELSE|END|EXTERNAL|FOR|FORWARD|FUNCTION|GOTO|IF|IMPLEMENTATION|IN|INLINE|INTERFACE|INTERRUPT|LABEL|MOD|NOT|OBJECT|OF|OR|PACKED|PROCEDURE|PROGRAM|RECORD|REPEAT|SET|SHL|SHR|THEN|TO|TYPE|UNIT|UNTIL|USES|VAR|VIRTUAL|WHILE|WITH|XOR)\b/i,null],[PR.PR_LITERAL,/^(?:true|false|self|nil)/i,null],[PR.PR_PLAIN,/^[a-z][a-z0-9]*/i,null],[PR.PR_LITERAL,/^(?:\$[a-f0-9]+|(?:\d+(?:\.\d*)?|\.\d+)(?:e[+\-]?\d+)?)/i,null,"0123456789"],[PR.PR_PUNCTUATION,/^.[^\s\w\.$@\'\/]*/,null]]),["pascal"]);
 //rebuild by neat 
